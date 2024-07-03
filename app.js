e1a0a5bd8e110515c8d56ad9c5c725fb5ac56f99
Yash Gaur <yashgaurcms@gmail.com>
2024-07-04 02:23:28 +0530

    irctc apis

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const trainRoutes = require('./routes/trainRoutes');
+const bookingRoutes=require('./routes/bookingRoutes')
 const sequelize = require('./config/database');
 
 require('dotenv').config();
@@ -11,7 +14,9 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use('/api/users', userRoutes);
-
+app.use('/api/admin', adminRoutes);
+app.use('/api/trains', trainRoutes);
+app.use('/api/bookings', bookingRoutes);
 
 sequelize.sync({ alter: true }).then(() => {
   app.listen(port, () => {
